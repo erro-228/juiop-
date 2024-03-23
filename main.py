a11d5d2ea18e8461ef24d61e27f65aa760a8ad87
erro-228 <80387470+erro-228@users.noreply.github.com>
2024-03-23 16:31:04 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,63 @@
+from telebot import TeleBot
+import db
+from time import sleep
+
+
+game = False
+TOKEN = "6721478570:AAFbRlZQcsySyjuzbGN7ykLA2m-mol6rfaA"
+bot = TeleBot(TOKEN)
+
+
+
+
+@bot.message_handler(func=lambda m: m.text.lower() == 'готов играть' and m.chat.type == 'private')
+def send_text(message):
+    bot.send_message(message.chat.id, f'{message.from_user.first_name} играет')
+    bot.send_message(message.from_user.id, 'Вы добавлены в игру')
+    db.insert_player(message.from_user.id,
+                     username=message.from_user.first_name)
+
+
+
+
+@bot.message_handler(commands=["play"])
+def game_on(message):
+    if not game:
+        bot.send_message(
+            message.chat.id, text='Если хотите играть напишите "готов играть" в лс')
+
+
+
+
+@bot.message_handler(commands=["game"])
+def game_start(message):
+    global game
+    players = db.players_amount()
+    if players >= 5 and not game:
+        db.set_roles(players)
+        players_roles = db.get_players_roles()
+        mafia_usernames = db.get_mafia_usernames()
+        for player_id, role in players_roles:
+            bot.send_message(player_id, text=role)
+            if role == 'mafia':
+                bot.send_message(player_id,
+                                 text=f'Все члены мафии:\n{mafia_usernames}')
+        game = True
+        bot.send_message(message.chat.id, text='Игра началась!')
+        return
+    bot.send_message(message.chat.id, text='недостаточно людей!')
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    helo = """
+Напиши - 'готов играть' для добавления в очередь
+
+/game - для старта игры
+"""
+    bot.send_message(message.chat.id, helo)
+
+
+
+if __name__ == "__main__":
+    bot.infinity_polling()
