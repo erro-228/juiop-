25420eee83a227145d0b7f54054697e9668dfc3b
erro-228 <80387470+erro-228@users.noreply.github.com>
2024-03-30 15:14:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,17 @@ from time import sleep
 game = False
 TOKEN = "TOKEN from BotFather"
 bot = TeleBot(TOKEN)
+night = False
+
+
+
+
+def get_killed(night):
+    if not night:
+        username_killed = db.citizens_kill()
+        return f'Горожане выгнали: {username_killed}'
+    username_killed = db.mafia_kill()
+    return f'Мафия убила: {username_killed}'
 
 
 
@@ -42,10 +53,52 @@ def game_start(message):
             if role == 'mafia':
                 bot.send_message(player_id,
                                  text=f'Все члены мафии:\n{mafia_usernames}')
+        db.clear(dead=True)
         game = True
         bot.send_message(message.chat.id, text='Игра началась!')
         return
     bot.send_message(message.chat.id, text='недостаточно людей!')
+
+
+
+
+@bot.message_handler(commands=["kill"])
+def kill(message):
+    username = ' '.join(message.text.split(' ')[1:])
+    usernames = db.get_all_alive()
+    mafia_usernames = db.get_mafia_usernames()
+    if night and message.from_user.first_name in mafia_usernames:
+        if not username in usernames:
+            bot.send_message(message.chat.id, 'Такого имени нет')
+            return
+        voted = db.vote("mafia_vote", username, message.from_user.id)
+        if voted:
+            bot.send_message(message.chat.id, 'Ваш голос учитан')
+            return
+        bot.send_message(
+            message.chat.id, 'У вас больше нет права голосовавать')
+    bot.send_message(message.chat.id, 'Сейчас нельзя убивать')
+
+
+
+
+@bot.message_handler(commands=["kick"])
+def kick(message):
+    username = ' '.join(message.text.split(' ')[1:])
+    usernames = db.get_all_alive()
+    if not night:
+        if not username in usernames:
+            bot.send_message(message.chat.id, 'Такого имени нет')
+            return
+        voted = db.vote("citizen_vote", username, message.from_user.id)
+        if voted:
+            bot.send_message(message.chat.id, 'Ваш голос учитан')
+            return
+        bot.send_message(
+            message.chat.id, 'У вас больше нет права голосовать')
+        return
+    bot.send_message(
+        message.chat.id, 'Сейчас ночь вы не можете никого выгнать')
 
 
 
